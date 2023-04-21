82225959aff9cb86d18b882bc29dc9c598bea40d
WangTaiYu1 <102717578+WangTaiYu1@users.noreply.github.com>
2023-04-21 14:41:12 +1000

    finish wall follow

--- scripts/wall_follow.py
+++ scripts/wall_follow.py
@@ -29,17 +29,18 @@ state_dict_ = {
     0: 'find the wall',
     1: 'turn left',
     2: 'follow the wall',
+    3: 'avoid the wall'
 }
 
 #define laser regions
 def clbk_laser(msg):
     global regions_
     regions_ = {
-        'right': min(min(msg.ranges[0:143]),10),
-        'fright': min(min(msg.ranges[144:287]),10),
-        'front': min(min(msg.ranges[288:431]),10),
-        'fleft': min(min(msg.ranges[432:575]),10),
-        'left': min(min(msg.ranges[576:713]),10),
+        'front': min(min(msg.ranges[0:100]),10),
+        'fright': min(min(msg.ranges[1500:1550]),10),#same as fleft
+        'right': min(min(msg.ranges[1400:1500 ]),10),#not change noidea
+        'fleft': min(min(msg.ranges[200:230]),10),
+        'left': min(min(msg.ranges[430:550]),10),
     }
     # rospy.loginfo(regions_)
     take_action()
@@ -60,38 +61,39 @@ def take_action():
 
     state_description = ''
 
-    d = 0.3
+    d = 1.2
+    front_distance = 0.9
 
     #no obstacles detected switch to state Find the wall
-    if regions['front'] > d and regions['fleft'] > d and regions['fright'] > d:
+    if regions['front'] > front_distance and regions['fleft'] > d and regions['fright'] > d:
         state_description = 'case 1 - nothing'
         change_state(0)
     # obstacles in the front switch to state turn left
-    elif regions['front'] < d and regions['fleft'] > d and regions['fright'] > d:
+    elif regions['front'] < front_distance and regions['fleft'] > d and regions['fright'] > d:
         state_description = 'case 2 - front'
         change_state(1)
     # obstacles in the right switch to state follow the wall
-    elif regions['front'] > d and regions['fleft'] > d and regions['fright'] < d:
+    elif regions['front'] > front_distance and regions['fleft'] > d and regions['fright'] < d:
         state_description = 'case 3 - fright'
-        change_state(2)
+        change_state(3)
     # obstacles in the left switch to state find the wall
-    elif regions['front'] > d and regions['fleft'] < d and regions['fright'] > d:
+    elif regions['front'] > front_distance and regions['fleft'] < d and regions['fright'] > d:
         state_description = 'case 4 - fleft'
         change_state(0)
     # obstacles in the front and  right switch to state turn left
-    elif regions['front'] < d and regions['fleft'] > d and regions['fright'] < d:
+    elif regions['front'] < front_distance and regions['fleft'] > d and regions['fright'] < d:
         state_description = 'case 5 - front and fright'
-        change_state(1)
+        change_state(3)
     # obstacles in the front and left switch to state turn left
-    elif regions['front'] < d and regions['fleft'] < d and regions['fright'] > d:
+    elif regions['front'] < front_distance and regions['fleft'] < d and regions['fright'] > d:
         state_description = 'case 6 - front and fleft'
         change_state(1)
     # obstacles in the front and left and right switch to state turn left
-    elif regions['front'] < d and regions['fleft'] < d and regions['fright'] < d:
+    elif regions['front'] < front_distance and regions['fleft'] < d and regions['fright'] < d:
         state_description = 'case 7 - front and fleft and fright'
         change_state(1)
     # obstacles in the left and right  switch to state find wall
-    elif regions['front'] > d and regions['fleft'] < d and regions['fright'] < d:
+    elif regions['front'] > front_distance and regions['fleft'] < d and regions['fright'] < d:
         state_description = 'case 8 - fleft and fright'
         change_state(0)
     else:
@@ -102,38 +104,50 @@ def take_action():
 def find_wall():
     msg = Twist()
     msg.linear.x = 0.1
-    msg.angular.z = -0.15
+    msg.angular.z = -0.3
     return msg
 #turn left
 def turn_left():
     msg = Twist()
-    msg.angular.z = 0.15
+    msg.angular.z = 0.5
     return msg
 #go stright
 def follow_the_wall():
     global regions_
-    
     msg = Twist()
     msg.linear.x = 0.1
     return msg
 
-def sign(sign):
-    if int(sign[0])==27:
-        start=True
+def avoid_wall():
+    msg  = Twist()
+    msg.linear.x = 0.1
+    msg.angular.z = 0.4
+    return msg
+
+def go_straight():
+    msg = Twist()
+    msg.linear.x = 0.1
+    return msg
+
     
+def sign(sign):
+    # rospy.loginfo('sign;')
+    # rospy.loginfo(sign)
+    if int(sign.data[0])==26:
+        start=True
     
     
 def main():
     global pub_
     
     rospy.init_node('reading_laser')
-    
+    start = True
     pub_ = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
     sub = rospy.Subscriber('/scan', LaserScan, clbk_laser)
     signs = rospy.Subscriber("/objects", Float32MultiArray, sign)
     rate = rospy.Rate(20)
     while not rospy.is_shutdown():
-        while start:
+        while start:        
             msg = Twist()
             if state_ == 0:
                 msg = find_wall()
@@ -141,6 +155,8 @@ def main():
                 msg = turn_left()
             elif state_ == 2:
                 msg = follow_the_wall()
+            elif state_ == 3:
+                msg = avoid_wall()
                 pass
             else:
                 rospy.logerr('Unknown state!')
